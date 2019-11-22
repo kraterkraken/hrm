af658d4a0dad0b9d1c3942631f8c4bbab193446f
kraterkraken <47699838+kraterkraken@users.noreply.github.com>
2019-11-21 19:19:01 -0500

    initial working checkin

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# hrm
+
 Interpreter for a programming language based on the game "Human Resource Machine"
+
+Usage:
+
+    hrm.py [options] sourcefile
+    Options:
+        -d datafile Sets the interpreter to read input data from the datafile
+                    (note: the "data" instruction will override data in the datafile)
+        -m memsize     Sets the size of temporary memory (default is 25)
+        -i inboxmode        
+                    Tells the interpreter how to handle the situation where
+                    an "inbox" instruction is encountered, but there is no more
+                    data to read:
+                    WARN = continue executing but display a warning (DEFAULT)
+                    STOP = gracefully stop execution
+                    QUERY = Query the user for the additional input
+        -h          Display help.
+
+Language Instruction Set:
+
+    data valuelist  uses a whitespace separated list of values as the input data
+                    Example: data 1 3 9 'A' 7 -1 15 'C'
+                    Note: This command overrides data passed in via input file.
+
+    init t v        initialize temp memory location t with value v
+                    Example: init 15 123
+
+    name t n        gives symbolic name n to temp memory location t which allows
+                    $n to be used in place of t in all commands
+                    Example: name 15 foo
+
+    :tag            tags a position in the instruction set with a name that
+                    can be referenced by any of the 'jump' instructions (see below)
+
+    inbox           reads from input into working memory
+    outbox          outputs working memory
+    copyfrom t      copies value from temp memory location t into working memory
+    copyto t        copies value from working memory into temp memory location t
+    add t           working memory = working memory + value at temp memory location t
+    sub t           working memory = working memory - value at temp memory location t
+    bump+ t         increment value at temp memory location t
+    bump- t         decrement value at temp memory location t
+    jump tag        jump to the given tag
+    jumpneg tag     jump to the given tag if value in working memory < 0
+    jumpzero tag    jump to the given tag if value in working memory == 0
+
+
+    t is any numeric value from 0 o 24
+    n is any string starting with a letter, followed by any number of
+      alphanumerics or underscores
+
+Note:
+    If instruction argument t above is surrounded by [], then dereference.
+    Example: Let temp memory location 15 hold the value 7.
+    Then "copyto [15]" will copy working memory to temp memory location 7.
+
+There is one instruction per line.
+Empty lines and lines starting with # are ignored.
+Temp memory has room for 25 values (0-24).  It is initialized to zeros.
+Working memory has room for 1 value, and is initialized to zero.
---
+++ hrm.py
@@ -0,0 +1,258 @@
+import sys
+import re
+import optparse
+################################################################################
+#
+#   HrmInterpreter class
+#   Parses and executes HRM commands
+#
+################################################################################
+class HrmInterpreter:
+    # -------------------------------------------------------------------------
+    def __init__(self, instructions_string, data_string, memsize, inboxmode):
+        # set up virtual machine
+        self.inbox_mode = inboxmode
+        self.temp = [0 for i in range(memsize)]
+        self.working = 0
+        self.ip = 0 # instruction pointer
+        self.dp = 0 # input data pointer
+        self.instructions = instructions_string.split("\n")
+        self.tags = dict([(b,a) for (a,b) in list(enumerate(self.instructions)) if b[:1] == ":"])
+        self.data = [eval(x) for x in data_string.split()]
+        self.names = {}
+        self.stats = {"size":0, "steps":0}
+
+        self.stats["size"] = len(
+            [0 for a in self.instructions
+            if a[:3] in ("inb", "out", "cop", "add", "sub", "bum", "jum")]
+        )
+
+    # -------------------------------------------------------------------------
+    def get_location(self, loc_string):
+
+        number = "\d+"  # 1+ digits
+        name = "[a-zA-Z][0-9a-zA-Z_]*"  # 1 alpha and 0+ AlphaNumUnderscores
+
+        address_re = "^(" + number + ")$"               # group 1: number
+        address_deref_re = "^\[(" + number + ")\]$"     # group 2: [number]
+        symbol_re = "^\$(" + name + ")$"                # group 3: $name
+        symbol_deref_re = "^\[\$(" + name + ")\]$"      # group 4: [$name]
+
+        location_re = address_re + "|" + address_deref_re + "|" + symbol_re + "|" + symbol_deref_re
+
+        m = re.match(location_re, loc_string)
+        if m:
+            val = None
+            if m.group(1) != None:
+                # we got a numeric memory address
+                val = int(m.group(1))
+            elif m.group(2) != None:
+                # we got a pointer, so dereference it
+                val = int(self.temp[int(m.group(2))])
+            elif m.group(3) != None:
+                # we got a symbolic memory address
+                val = int(self.names[m.group(3)])
+            elif m.group(4) != None:
+                # we got a symbolic pointer, so dereference it
+                val = int(self.temp[int(self.names[m.group(4)])])
+
+            if val == None or val < 0 or val > 24:
+                sys.exit("Error: invalid memory address {}, should be between 0 and 24.".format(val))
+            else:
+                return val
+        else:
+            sys.exit("Syntax error: '" + loc_string + "' is not a valid location.'")
+
+    # -------------------------------------------------------------------------
+    def get_args(self, tokens, arg_types=[]):
+
+        args = tokens[1:]
+
+        if len(args) != len(arg_types):
+            sys.exit(
+                "Error on line {}:"
+                " Instruction '{}' should have had {} arguments."
+                "  It had {}.".format(
+                    self.ip+1, tokens[0], len(arg_types), len(args)
+                    )
+                )
+
+        if len(args) == 0: return []
+
+        parse_list = zip(arg_types, args)
+        res = []
+        for type,arg in parse_list:
+            if type == "location":
+                res.append(self.get_location(arg))
+            elif type =="value":
+                res.append(arg)
+            elif type == "name":
+                res.append(arg)
+            elif type == "tag":
+                res.append(arg)
+            else:
+                sys.exit("Error: bad argument type '{}'".format(type))
+                pass
+
+        return res
+
+    # -------------------------------------------------------------------------
+    def run(self):
+        # execute the HRM program until there are no more instructions
+        while self.ip < len(self.instructions):
+
+            # skip blank lines, comments, and tagged lines
+            if len(self.instructions[self.ip]) == 0 or self.instructions[self.ip][0] in ('#', ':'):
+                self.ip += 1
+                continue
+
+            # perform the given instruction
+            tokens = self.instructions[self.ip].split()
+            instruction = tokens[0]
+
+            if instruction == "inbox":
+                self.stats["steps"] += 1
+                self.get_args(tokens)
+                d = None
+                if self.dp >= len(self.data):
+                    if self.inbox_mode == "WARN":
+                        print("Warning: attempting to input data, but there is no more.")
+                    elif self.inbox_mode == "STOP":
+                        sys.exit("Tried to read data that is not there ... exiting.")
+                    elif self.inbox_mode == "QUERY":
+                        d = input("Enter a value for the inbox instruction:")
+                else:
+                    d = self.data[self.dp]
+                    self.dp += 1
+
+                self.working = d
+
+            elif instruction == "outbox":
+                self.stats["steps"] += 1
+                self.get_args(tokens)
+                print(self.working)
+
+            elif instruction == 'data':
+                data_string = " ".join(tokens[1:])
+                self.data = [eval(x) for x in data_string.split()]
+
+            elif instruction == "copyfrom":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.working = self.temp[arg]
+
+            elif instruction == "copyto":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.temp[int(arg)] = self.working
+
+            elif instruction == "bump+":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.temp[int(arg)] += 1
+
+            elif instruction == "bump-":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.temp[int(arg)] -= 1
+
+            elif instruction == "add":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.working += temp[arg]
+
+            elif instruction == "sub":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["location"])
+                self.working -= temp[arg]
+
+            elif instruction == "init":
+                [loc, val] = self.get_args(tokens, ["location", "value"])
+                self.temp[int(loc)] = val
+
+            elif instruction == "name":
+                [loc, name] = self.get_args(tokens, ["location", "name"])
+                self.names[name] = loc
+
+            elif instruction == "jump":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["tag"])
+                self.ip = self.tags[arg]
+                continue
+
+            elif instruction == "jumpneg":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["tag"])
+                if self.working < 0:
+                    self.ip = self.tags[arg]
+                    continue
+
+            elif instruction == "jumpzero":
+                self.stats["steps"] += 1
+                [arg] = self.get_args(tokens, ["tag"])
+                if self.working == 0:
+                    self.ip = self.tags[arg]
+                    continue
+
+            else:
+                sys.exit("Error on line {}: Bad instruction '{}'".format(self.ip+1, instruction))
+
+            self.ip += 1
+# end class definition
+
+
+################################################################################
+#
+#   HrmOptionManager class
+#   Handles all of the command line arguments and options
+#
+################################################################################
+class HrmOptionManager:
+    def __init__(self):
+        parser = optparse.OptionParser(usage="%prog [options] sourcefile")
+        parser.add_option("-d", type="string", dest="datafile",
+            help="specifies the file that the interpreter should use as input data for the program it is interpreting")
+        parser.add_option("-m", type="int", dest="memsize", default=25,
+            help="sets the size of temporary memory (default is 25)")
+        parser.add_option("-i", type="choice", dest="inboxmode",
+            choices=["WARN","STOP","QUERY"], default="WARN",
+            help="tells the interpreter how to handle the situation where "
+            "an 'inbox' instruction is encountered, but there is no more data to read:\n"
+            "WARN = continue executing but display a warning (DEFAULT). "
+            "STOP = gracefully stop execution. "
+            "QUERY = Query the user for the additional input.")
+        (self.options, pos_args) = parser.parse_args()
+
+        if len(pos_args) < 1:
+            parser.error("missing name of sourcefile")
+        elif len(pos_args) > 1:
+            parser.error("too many command line arguments")
+        else:
+            self.sourcefile = pos_args[0]
+
+        # read instructions from source file
+        self.instructions_string = ""
+        with open(self.sourcefile) as file:
+            self.instructions_string = file.read()
+
+        # read input data from data file (if provided)
+        self.data_string = ""
+        if self.options.datafile != None:
+            with open(self.options.datafile) as file:
+                self.data_string = file.read()
+# end class definition
+
+################################################################################
+#
+#   Main Program
+#
+################################################################################
+
+opman = HrmOptionManager()
+hrm = HrmInterpreter(
+    opman.instructions_string,
+    opman.data_string,
+    opman.options.memsize,
+    opman.options.inboxmode)
+hrm.run()
+print(hrm.stats)
